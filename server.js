a0ab637454d92b14dff59952a888381649f93ab2
play-and-win <playandwina@gmail.com>
2026-07-14 21:48:22 +0530

    Implement '/test' endpoint to load library
    Added a new '/test' endpoint that loads a library and returns its exports.

--- server.js
+++ server.js
@@ -5,13 +5,32 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-
     res.send("FF API Test Server Running");
+});
+
+app.get("/test", async (req, res) => {
+
+    try {
+
+        const ff = require("@pure0cd/freefire-api");
+
+        res.json({
+            success: true,
+            message: "Library Loaded Successfully",
+            exports: Object.keys(ff)
+        });
+
+    } catch (e) {
+
+        res.json({
+            success: false,
+            error: e.message
+        });
+
+    }
 
 });
 
 app.listen(PORT, () => {
-
     console.log("Server Started");
-
 });
